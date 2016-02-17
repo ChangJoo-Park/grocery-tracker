c9869c920904a59f9c2428d3c845700d0d1cd48d
ChangJoo Park <changjoo.park.cj@gmail.com>
2016-02-17 20:43:33 +0100

    Add route - list

--- app/transitions.js
+++ app/transitions.js
@@ -1,20 +1,19 @@
 export default function(){
-  // Will run if the current media type is mobile
+  let duration = 500;
   this.transition(
-    this.media('mobile'), // You could use a keyword or even a media query here
     this.fromRoute('lists.index'),
     this.toRoute('lists.new'),
     this.use('toUp'),
     this.reverse('toDown')
   );
 
-  // Will run if the current media type is desktop
   this.transition(
-    this.media('desktop'),
     this.fromRoute('lists.index'),
-    this.toRoute('lists.new'),
-    this.use('toDown'),
-    this.reverse('toLeft')
+    this.toRoute('lists.list'),
+    this.use('fade'),
+    this.reverse('explode', {
+      use: 'fade'
+    })
   );
-
 };
+
