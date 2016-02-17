2cc7b2daa82daacf7c885f0e0e5fc7884b278f74
ChangJoo Park <changjoo.park.cj@gmail.com>
2016-02-17 05:16:02 +0100

    Initialize Firebase

---
+++ app/routes/application.js
@@ -0,0 +1,15 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  model() {
+    return this.store.findAll('list');
+  },
+  actions: {
+    save() {
+      Ember.Logger.info("SAVE");
+      let newList = this.get('currentModel');
+      Ember.Logger.info(newList);
+      newList.save();
+    }
+  }
+});
