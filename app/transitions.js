9f6113399c96f183b633c6789c854fa63b6aff66
ChangJoo Park <changjoo.park.cj@gmail.com>
2016-02-17 20:11:39 +0100

    Add Responsive

--- app/transitions.js
+++ app/transitions.js
@@ -1,8 +1,20 @@
 export default function(){
+  // Will run if the current media type is mobile
   this.transition(
+    this.media('mobile'), // You could use a keyword or even a media query here
     this.fromRoute('lists.index'),
     this.toRoute('lists.new'),
     this.use('toUp'),
     this.reverse('toDown')
   );
+
+  // Will run if the current media type is desktop
+  this.transition(
+    this.media('desktop'),
+    this.fromRoute('lists.index'),
+    this.toRoute('lists.new'),
+    this.use('toDown'),
+    this.reverse('toLeft')
+  );
+
 };
