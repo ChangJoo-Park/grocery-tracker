0a2908e098ea8bbfa2cd4e77d76c1b30e1370bc4
ChangJoo Park <changjoo.park.cj@gmail.com>
2016-02-18 23:43:49 +0100

    add navbar logo

--- app/components/nav-bar.js
+++ app/components/nav-bar.js
@@ -4,6 +4,9 @@ export default Ember.Component.extend({
   didInsertElement() {
     $(this.element).find(".button-collapse").sideNav();
   },
+  willInsertElement() {
+    $(this.element).find("#side-nav #logo");
+  },
   actions: {
   }
 });
